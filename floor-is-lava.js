8e4ab87d77369fb54ca26244b1e277c8bce8c192
AdrianCasadoAguilera <152631290+AdrianCasadoAguilera@users.noreply.github.com>
2024-12-22 15:24:01 +0100

    initial commit

---
+++ floor-is-lava.js
@@ -0,0 +1,96 @@
+const readline = require('readline').promises
+
+const tauler = [
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0],
+]
+
+let playerPos = "0,0"
+let vides = 32
+
+let continuePlaying = true;
+
+const printTaulerTrampa = () => {
+    for(let i = 0; i < tauler.length; i++) {
+        let line = "";
+        for(let j = 0; j<tauler[i].length; j++) {
+            if(tauler[i][j] === 0 ) {
+                line += "·"
+            }else {
+                line += "l"
+            }
+        }
+        console.log(line)
+    }
+}
+
+const printTauler = () => {
+    for(let i = 0; i < tauler.length; i++) {
+        let line = "";
+        for(let j = 0; j<tauler[i].length; j++) {
+            if(playerPos === `${i},${j}`) {
+                line += "T"
+            }
+            else if(tauler[i][j] === 0 || tauler[i][j]) {
+                line += "·"
+            }
+        }
+        console.log(line)
+    }
+}
+
+const initTauler = () => {
+    let lavaToPut = 16
+
+    while(lavaToPut > 0) {
+        const lavaPosX = Math.floor(Math.random()*6)
+        const lavaPosY = Math.floor(Math.random()*8)
+
+        if(tauler[lavaPosX][lavaPosX] === 0 && !(lavaPosX === 0 && lavaPosY === 0) && !(lavaPosX === 7 && lavaPosY === 5)){
+            tauler[lavaPosX][lavaPosY] = 1;
+            lavaToPut--;
+        }
+    }
+}
+
+const main = async () => {
+
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+
+    initTauler()
+    while(continuePlaying) {
+        printTauler()
+        const jugada = await rl.question("Escriu una comanda: ");
+
+        handleJugada(jugada);
+    }
+}
+
+const handleJugada = (jugada) => {
+    jugada = jugada.split(" ")
+    if(jugada.length == 2){
+        if(jugada[0] == "caminar"){
+            const posicio = jugada[1]
+            if(posicio === "dreta") {
+                const playerPosArray = playerPos.split(",")
+                playerPos = `${playerPosArray[0]},${parseInt(playerPosArray[1])+1}`
+                console.log(playerPos)
+            }else if(posicio === "esquerra") {
+
+            }else if(posicio === "avall") {
+
+            }else if(posicio === "amunt") {
+
+            }
+        }
+    }
+}
+
+main()
